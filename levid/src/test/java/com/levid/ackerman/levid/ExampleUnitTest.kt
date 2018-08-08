66c4b24d282884a4baa9dca069822255141e94c9
DavidBarbaran <alejandro.david.barbaran@gmail.com>
2018-08-08 18:02:23 -0500

    Refactorizar clases java a kotlin y modificar dependencias

---
+++ levid/src/test/java/com/levid/ackerman/levid/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package com.levid.ackerman.levid
+
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
